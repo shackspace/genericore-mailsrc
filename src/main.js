4b5cc505f0322a41c49b27798fb0590f8d438797
tv <tv@also>
2011-01-15 11:03:46 +0100

    main: use externalized amqp and tcp

---
+++ src/main.js
@@ -0,0 +1,17 @@
+//
+//
+//
+
+var config = require('./' + process.argv[2]);
+
+var parse = require('./email').parse;
+var amqp = require('./amqp').createClient(config.amqp);
+var tcp = require('./tcp').createServer(config.tcp);
+
+amqp.connect(function () {
+  tcp.serve(function (message) {
+    var data = parse(message);
+    console.log('publishing: ' + data['Header-Fields'].From);
+    amqp.publish({ type: 'mail', subtype: 0, data: data });
+  });
+});
