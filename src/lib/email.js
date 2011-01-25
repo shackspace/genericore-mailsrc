eb31760a985eb4f1d8a8cec4cfc9ac2bf2a494e1
tv <tv@also>
2011-01-25 21:13:25 +0100

    email: fixed header field parser

--- src/lib/email.js
+++ src/lib/email.js
@@ -7,7 +7,8 @@ exports.parse = function (s) {
   var hfs = {};
   s_hfs.replace(/\n\s+/g, ' ').split(/\n/g).forEach(function (hf_s) {
     var hf = hf_s.split(/:\s*/, 2);
-    var name = hf[0], value = hf[1];
+    var name = hf[0];
+    var value = [hf[1]].concat(hf_s.split(/:/).splice(2)).join(':');
     hfs[name] = value;
   });
   return { "Header-Fields": hfs, "Body": b };
