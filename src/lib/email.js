516ad2073b57fadb4b609c203a48b9fc15a0ef16
tv <tv@also>
2011-01-25 21:43:02 +0100

    email: added Date parser

--- src/lib/email.js
+++ src/lib/email.js
@@ -1,4 +1,18 @@
 
+var parsers = {
+  Date: function (string) {
+    var date = new Date(string);
+    return {
+      year:         date.getUTCFullYear(),
+      month:        date.getUTCMonth() + 1,
+      day:          date.getUTCDate(),
+      hour:         date.getUTCHours(),
+      minute:       date.getUTCMinutes(),
+      second:       date.getUTCSeconds()
+    };
+  }
+};
+
 exports.parse = function (s) {
   var s_hfs_b = s.split("\n\n");
   var s_hfs = s_hfs_b[0];
@@ -11,6 +25,10 @@ exports.parse = function (s) {
     var value = [hf[1]].concat(hf_s.split(/:/).splice(2)).join(':');
     hfs[name] = value;
   });
+  for (key in parsers) {
+    var parser = parsers[key];
+    hfs[key] = parser(hfs[key]);
+  };
   return { "Header-Fields": hfs, "Body": b };
 };
 
